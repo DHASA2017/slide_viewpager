a92413f8a46f33fefa788ebfdc4731db9d884967
liulin <liulin@jiuqi.com.cn>
2017-06-07 18:38:04 +0800

    添加一个状态栏工具，并应用到栗子主页面

--- app/src/main/java/com/dl/commonviewpager/activity/MainActivity.java
+++ app/src/main/java/com/dl/commonviewpager/activity/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StatusBarUtils.setFullScreen(this);
+//        StatusBarUtils.setFullScreen(this);
         if(getSupportActionBar()!=null){
             getSupportActionBar().hide();
         }
