73f7baebecee6eaf61e1116c0c186bb1c388f31e
liulin <liulin@jiuqi.com.cn>
2017-06-07 18:36:36 +0800

    添加一个状态栏工具，并应用到栗子主页面

--- app/src/main/java/com/dl/commonviewpager/activity/MainActivity.java
+++ app/src/main/java/com/dl/commonviewpager/activity/MainActivity.java
@@ -17,6 +17,7 @@ import com.dl.commonviewpager.R;
 import com.dl.commonviewpager.interfaces.ViewPagerHolder;
 import com.dl.commonviewpager.interfaces.ViewPagerHolderCreator;
 import com.dl.commonviewpager.widget.CommonViewPager;
+import com.dl.commonviewpager.widget.StatusBarUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,18 +31,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-            window.setNavigationBarColor(Color.TRANSPARENT);
-        }
         setContentView(R.layout.activity_main);
+        StatusBarUtils.setFullScreen(this);
         if(getSupportActionBar()!=null){
             getSupportActionBar().hide();
         }
